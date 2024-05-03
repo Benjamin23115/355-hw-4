1cf04b10dc8f530647cfb79383b86028cb3f0502
Benjamin23115 <benjaminfarias313@gmail.com>
2024-05-03 14:18:19 -0700

    changes
    Co-authored-by: Joel J Aleman <joel.aleman7@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -7,22 +7,22 @@
 #include <ctime>
 #include <time.h>
 
-
 using namespace std;
 
 const int UNLOCKED = 0;
 const int LOCKED = 1;
 const int NUMBER_OF_PHILOSPHERS = 5;
 
-// Hi Luis, this is the dead lock version
 class Chopstick
 {
 private:
     mutex chopTex;
     int status;
     int id;
+
 public:
-    Chopstick(){
+    Chopstick()
+    {
     }
     Chopstick(int id)
     {
@@ -47,6 +47,7 @@ private:
     Chopstick chopsticks[NUMBER_OF_PHILOSPHERS]; // Array of Chopsticks
     bool status = false;
     mutex statusMutex;
+
 public:
     Syncro()
     {
@@ -60,26 +61,31 @@ public:
     {
         chopsticks[id].lockChopstick();
     }
-    // command to grab both chopsticks
     void getChopsticks(int id)
     {
-        pickUpChopstick(id);
-        pickUpChopstick((id + 1) % NUMBER_OF_PHILOSPHERS);
+        // Pick up chopsticks in ascending order
+        if (id == 0)
+        {
+            pickUpChopstick(id);
+            pickUpChopstick((id + 1) % NUMBER_OF_PHILOSPHERS);
+        }
+        else
+        {
+            pickUpChopstick((id + 1) % NUMBER_OF_PHILOSPHERS);
+            pickUpChopstick(id);
+        }
     }
-    // command to release both chopsticks
     void releaseChopsticks(int id)
     {
         putDownChopstick(id);
         putDownChopstick((id + 1) % NUMBER_OF_PHILOSPHERS);
     }
-    // Sends Activation signal to all philosophers
     void setStatus(bool status)
     {
         statusMutex.lock();
         this->status = status;
         statusMutex.unlock();
     }
-    // Check condition of Status
     bool getStatus()
     {
         statusMutex.lock();
@@ -100,19 +106,18 @@ public:
 class Philosopher
 {
 private:
-    
     string name;
-    int state;// 0:thinking, 1:hungry, 2:eating, 3:starved 
+    int state;
     int id;
     Chopstick *left, *right;
     thread philThread;
     mutex outputMutex;
-    Chopstick *left, *right;
     Syncro &syncro;
+    double thinkTime;
     double hungerTime;
+    double eatTime;
 
 public:
-    // inititalize name, monitor, ID, leftChopstick, rightChopstick, philThread
     Philosopher(string name, Syncro &t, int id, Chopstick &leftChopstick,
                 Chopstick &rightChopstick) : philThread(&Philosopher::run, this), syncro(t)
     {
@@ -121,115 +126,131 @@ public:
         this->left = &leftChopstick;
         this->right = &rightChopstick;
         this->state = 0;
+        this->thinkTime = 0.0;
+        this->hungerTime = 0.0;
+        this->eatTime = 0.0;
     }
 
     ~Philosopher()
     {
         philThread.join();
     }
-    // Threaded operation for Philosopher
+
     void run()
     {
         do
         {
             cout << "waiting for philosopher initialization." << endl;
         } while (syncro.getStatus() == false);
-        
-        // Run while initialized
+
         while (syncro.getStatus() == true)
         {
             usleep(50000);
-            if(this->state == 0){
+            if (this->state == 0)
+            {
                 thinking();
-            } else if(this->state == 1){
-                time_t start = time(0);
-                hungry(start);
-            } else if(this->state == 2){
+            }
+            else if (this->state == 1)
+            {
+                hungry();
+            }
+            else if (this->state == 2)
+            {
                 eating();
-            } else if(this->state == 3){
-                cout << this->name << " HAS STARVED MFERS" << endl;
+            }
+            else if (this->state == 3)
+            {
+                cout << this->name << " has starved." << endl;
+                syncro.setStatus(false);
             }
         }
     }
-    
+
     void thinking()
     {
-        // Allow philosopher think for a random amount of time
-        cout << this->name << " " << this->id << ": is thinking." << endl;
+        time_t start = time(0);
+        // cout << this->name << " " << this->id << ": is thinking." << endl;
         usleep(500000);
         if (this->coinToss() == 1)
         {
-            // Philosopher is now hungry
             this->state = 1;
         }
+        this->thinkTime += this->thinkTime + difftime(time(0), start);
     }
-    // Start and track hunger timer;
-    void hungry(time_t start)
+
+    void hungry()
     {
-        cout << this->name << " " << this->id << ": is hungry." << endl;
-        if(coinToss()){
+        time_t start = time(0);
+        // cout << this->name << " " << this->id << ": is hungry." << endl;
+        if (coinToss())
+        {
             syncro.getChopsticks(this->id);
             this->state = 2;
             return;
         }
-        if (this->hungerTime > 1.0){
+        if (this->hungerTime > 1.0)
+        {
             this->state = 3;
         }
         this->hungerTime += this->hungerTime + difftime(time(0), start);
     }
-    // Randomizer to determine if Philosopher drops chopstick
+
     int coinToss()
     {
         return rand() % 2 == 0 ? 0 : 1;
     }
-    // Philosopher has access to both chopsticks and will eat
+
     void eating()
     {
-        cout << this->name << " " << this->id << " is eating." << endl;
+        time_t start = time(0);
+        // cout << this->name << " " << this->id << " is eating." << endl;
         usleep(500000);
         if (this->coinToss() == 1)
         {
             syncro.releaseChopsticks(this->id);
-            cout << this->name << " " << this->id << " has finished eating." << endl;
+            // cout << this->name << " " << this->id << " has finished eating." << endl;
             this->state = 0;
         }
+        this->eatTime += this->eatTime + difftime(time(0), start);
     }
-    int test(int state){
-        return this->state;
+    void getStats()
+    {
+        cout << this->name << " " << this->id << " thought for: " << this->thinkTime << "milli-seconds" << endl;
+        cout << this->name << " " << this->id << " was hungry for: " << this->hungerTime << " milli-Seconds" << endl;
+        cout << this->name << " " << this->id << " Ate for: " << this->eatTime << " milli-Seconds" << endl;
     }
 };
 
-// Array used to inititalize Philosopher names
 const string nameArray[] =
     {"Yoda", "Obi-Wan", "Rey", "Kanan", "Leia", "Luke", "Ahsoka",
      "Mace Windu", "Ezra", "Palpatine", "Anakin", "Kylo Ren", "Dooku",
      "Kit Fitso", "Luminara", "Plo Koon", "Revan", "Thrawn", "Zeb", "Sabine"};
 
-int main() {
-    // Syncro is thread monitor
+int main()
+{
     Syncro *syncro = new Syncro();
-    // Set equal number of Chopsticks and Philosophers
     Chopstick *chopsticks[NUMBER_OF_PHILOSPHERS];
     Philosopher *philosophers[NUMBER_OF_PHILOSPHERS];
-    int lastPhilosopher = NUMBER_OF_PHILOSPHERS - 1;
-    
-    // Declare Chopsticks and Philosophers
-    for (int i = 0; i < NUMBER_OF_PHILOSPHERS; i++) {
+
+    for (int i = 0; i < NUMBER_OF_PHILOSPHERS; i++)
+    {
         chopsticks[i] = new Chopstick(i);
         philosophers[i] = new Philosopher(nameArray[i], *syncro, i, *chopsticks[i], *chopsticks[(i + 1) % NUMBER_OF_PHILOSPHERS]);
     }
 
-    // Set syncro to true to start all philosophers
-    syncro->setStatus(true);
-    usleep(1000000000000000000000000);
+    unsigned int twntyMin = 20 * 60 * 1000000;
 
-    // Set syncro to false to stop all philosophers
+    syncro->setStatus(true);
+    usleep(twntyMin);
+
     syncro->setStatus(false);
-    
-    // Free the memory
-    for(int i = 0; i < NUMBER_OF_PHILOSPHERS; i++){
-        delete(chopsticks[i]);
-        delete(philosophers[i]);
+
+    // go through the list of philosophers
+    for (int i = 0; i < NUMBER_OF_PHILOSPHERS; i++)
+    {
+        philosophers[i]->getStats();
+        delete (chopsticks[i]);
+        delete (philosophers[i]);
     }
     return 0;
 }
